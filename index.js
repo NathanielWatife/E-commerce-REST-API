2aec6b698868e33cfdf46efc4534344a920c69aa
NathanielWatife <awosanminathniel0@gmail.com>
2024-08-16 16:39:08 +0100

    changed port

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-import express from "express";
+const express = require("express");
 const app = express();
-import mongoose from "mongoose";
-// import { configDotenv } from "dotenv";
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 
+
 dotenv.config();
+
 mongoose.connect(process.env.MONGO_URL).then(() => console.log("DB-Connection good!!")).catch((err) => {
     console.log(err);
 });
@@ -17,9 +18,6 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 
 
-
-
-
-app.listen(process.env.PORT || 5000, ()=> {
-    console.log("Server running.........")
+app.listen(process.env.PORT || 5003, ()=> {
+    console.log("Back-end Server is running");
 });
