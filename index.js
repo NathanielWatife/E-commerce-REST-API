ec2b3abebe485576d78b326c935daa6be195b36a
NathanielWatife <awosanminathaniel0@gmail.com>
2025-04-22 20:39:15 +0100

    Made changes to directory

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-const express = require("express");
+import express from "express";
 const app = express();
-const mongoose = require("mongoose");
+
+
+import connectDB from "./config/db";
+import dotenv from "dotenv";
 const dotenv = require("dotenv");
-const userRoute = require("./routes/user");
-const authRoute = require("./routes/auth");
+
 
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL).then(() => console.log("DB-Connection good!!")).catch((err) => {
-    console.log(err);
-});
-
+connectDB();
 app.use(express.json());
 //  creating routes
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-
 
 app.listen(process.env.PORT || 5003, ()=> {
     console.log("Back-end Server is running");
