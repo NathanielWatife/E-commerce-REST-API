6468c474f2a0d41a45fbc36a875849b4390c5959
NathanielWatife <awosanminathaniel0@gmail.com>
2025-05-09 17:18:33 +0100

    Added routes to server

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectDB } from './config/db.js';
-import authRoutes  from './routes/authRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import cookieParser from "cookie-parser";
+import authRoutes  from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 const app = express();
+
 // middleware
 app.use(express.json());
-//  creating routes
-app.use("/api/auth", authRoutes);
+app.use.apply(cookieParser());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || "*",
+  credentials: true
+}))
 
-app.listen(process.env.PORT, () => {
+//  routes
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+
+// error handling middleware
+app.user((err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+  res.status(statusCode)
+  res.json({
+    success: false,
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack
+  })
+})
+
+
+// not found error middleware handling
+app.use((req, res, next) => {
+  res.status(404).json({
+    sucess: false,
+    message: `Not found - ${req.originalUrl}`
+  })
+})
+
+
+
+// port 
+const PORT = process.env.PORT
+app.listen(PORT, () => {
   connectDB();
-  console.log(`Backend Server running on ${process.env.PORT}`);
+  console.log(`Backend Server running on ${PORT}`);
 });
