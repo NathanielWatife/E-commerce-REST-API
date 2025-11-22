2b6e6a554bec91f81ee612c6e63ac202f8a53867
NathanielWatife <awosanminathaniel0@gmail.com>
2025-11-22 21:35:23 +0100

    index fix

--- utils/generateTokenAndSetCookie.js
+++ utils/generateTokenAndSetCookie.js
@@ -18,8 +18,8 @@ const generateTokenAndSetCookie = (res, userId) => {
     res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
-        maxAge: 7 * 24 * 60 * 1000,
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return token;
 };
