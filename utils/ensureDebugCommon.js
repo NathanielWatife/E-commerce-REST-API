c3e25c550b6ccc4a6edd56e35f4f98078bb74f0d
NathanielWatife <awosanminathaniel0@gmail.com>
2025-11-22 01:12:05 +0100

    comment out

--- utils/ensureDebugCommon.js
+++ utils/ensureDebugCommon.js
@@ -1,6 +1,5 @@
-const setup = require("debug/src/common.js");
+// const setup = require("debug/src/common.js");
 
-// Exporting the boolean ensures the module is retained during bundling.
-const debugCommonLoaded = Boolean(setup);
+// const debugCommonLoaded = Boolean(setup);
 
-module.exports = { debugCommonLoaded };
+// module.exports = { debugCommonLoaded };
